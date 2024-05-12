db6249692a10238f556efa886834fe462fdf836e
Thomas Axelsson <thomasa88@gmail.com>
2024-05-12 09:59:06 +0200

    KeyHandler trait

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use evdev::{
     uinput::{VirtualDevice, VirtualDeviceBuilder},
-    Device, InputEvent, Key,
+    Device, EventType, InputEvent, Key,
 };
 
 // type Action = dyn Fn(&mut ProcState, &mut VirtualDevice);
@@ -12,9 +12,19 @@ fn ba<'v, 'p: 'v, T: Fn(ProcView<'v, 'p>)>(action: T) -> Box<T> {
     Box::new(action)
 }
 
+#[derive(Debug, PartialEq)]
+enum ProcessResult {
+    Consumed,
+    NotConsumed,
+}
+
 struct Layer<'a> {
-    key_presses: &'a [KeyPress],
-    chords: &'a [Chord],
+    handlers: &'a [&'a dyn KeyHandler],
+    // chords: &'a [Chord],
+}
+
+trait KeyHandler {
+    fn process_event(&self, event: &InputEvent, view: ProcView) -> ProcessResult;
 }
 
 struct KeyPress {
@@ -29,6 +39,23 @@ impl KeyPress {
             action: action,
         }
     }
+}
+
+impl KeyHandler for KeyPress {
+    fn process_event(&self, event: &InputEvent, view: ProcView) -> ProcessResult {
+        if event.code() == self.key.code() {
+            (self.action)(view);
+            ProcessResult::Consumed
+        } else {
+            ProcessResult::NotConsumed
+        }
+    }
+}
+
+enum KH {
+    Key(KeyPress),
+    UpKey(KeyPress),
+    Chord(Chord),
 }
 
 struct Chord {
@@ -66,35 +93,41 @@ impl<'a> Processor<'a> {
         //self.input_kb.get_key_state()
 
         for event in self.devices.input_kb.fetch_events()? {
-            Self::process(&mut self.state, &mut self.devices.virt_kb, event)?;
+            if event.event_type() == EventType::KEY {
+                Self::process_key_event(&mut self.state, &mut self.devices.virt_kb, event)?;
+            } else {
+                self.devices.virt_kb.emit(&[event])?;
+            }
         }
         Ok(())
     }
 
-    fn process(
+    fn process_key_event(
         state: &mut ProcState,
         virt_kb: &mut VirtualDevice,
         event: InputEvent,
     ) -> Result<ProcessResult> {
         if let Some(layer) = state.active_layer {
-            for kp in layer.key_presses {
-                (kp.action)(ProcView { state, virt_kb });
+            for kp in layer.handlers {
+                if kp.process_event(&event, ProcView { state, virt_kb }) == ProcessResult::Consumed
+                {
+                    break;
+                    // (kp.action)(ProcView { state, virt_kb });
+                }
             }
+            // for kp in layer.key_presses {
+            //     (kp.action)(ProcView { state, virt_kb });
+            // }
         }
         virt_kb.emit(&[event])?;
-        Ok(ProcessResult::None)
+        Ok(ProcessResult::Consumed)
     }
-}
-
-enum ProcessResult {
-    // KeyPressed(&'a KeyPress),
-    None,
 }
 
 fn main() -> Result<()> {
     // let space_chord = {}
 
-    const NAV_LAYER: Layer = Layer { key_presses: &[], chords: &[] };
+    const NAV_LAYER: Layer = Layer { handlers: &[] };
 
     // let num_key = KeyPress {
     //     key: Key::KEY_F,
@@ -114,8 +147,7 @@ fn main() -> Result<()> {
         }),
     );
     let first_layer = Layer {
-        key_presses: &[test_key],
-        chords: &[],
+        handlers: &[&test_key, &num_key],
     };
 
     let mut virt_kb = VirtualDeviceBuilder::new()?
