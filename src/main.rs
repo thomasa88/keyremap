95738be27a7a1bdbe20181f6b22fd3af7a8a9baf
Thomas Axelsson <thomasa88@gmail.com>
2024-05-13 19:29:23 +0200

    Layer ID

--- src/main.rs
+++ src/main.rs
@@ -1,176 +1,158 @@
+use std::{cell::OnceCell, marker::PhantomData, sync::Once, vec};
+
 use anyhow::Result;
 use evdev::{
     uinput::{VirtualDevice, VirtualDeviceBuilder},
-    Device, EventType, InputEvent, Key,
+    Device, Key,
 };
+use tracing_subscriber::layer;
 
-// type Action = dyn Fn(&mut ProcState, &mut VirtualDevice);
-type Action = dyn Fn(ProcView);
-type BoxAction = Box<Action>;
+type ActionFn = Box<dyn Fn(&mut Processor) -> Result<()>>;
 
-fn ba<'v, 'p: 'v, T: Fn(ProcView<'v, 'p>)>(action: T) -> Box<T> {
-    Box::new(action)
+struct Layer {
+    id: usize,
+    handlers: Vec<Box<dyn KeyEventHandler>>,
 }
 
-#[derive(Debug, PartialEq)]
-enum ProcessResult {
-    Consumed,
-    NotConsumed,
+impl Layer {
+    // fn new() -> Layer {
+    //     Layer {
+    //     }
+    // }
+
+    fn add_key_press(&mut self, key: Key, action: ActionFn) {
+        self.handlers.push(Box::new(KeyPress { key, action }));
+    }
 }
 
-struct Layer<'a> {
-    handlers: &'a [&'a dyn KeyHandler],
-    // chords: &'a [Chord],
+struct LayerFactory {
+    next_id: usize,
 }
 
-trait KeyHandler {
-    fn process_event(&self, event: &InputEvent, view: ProcView) -> ProcessResult;
+impl LayerFactory {
+    fn new() -> Self {
+        Self { next_id: 0 }
+    }
+
+    fn create_layer(&mut self) -> Layer {
+        let id = self.next_id;
+        self.next_id += 1;
+        Layer {
+            id,
+            handlers: vec![],
+        }
+    }
 }
 
 struct KeyPress {
     key: Key,
-    action: BoxAction,
+    action: ActionFn,
 }
 
-impl KeyPress {
-    fn new(key: Key, action: BoxAction) -> Self {
-        Self {
-            key,
-            action: action,
-        }
+impl KeyEventHandler for KeyPress {
+    fn handle_event(&self, proc: &mut Processor) -> Result<HandleResult> {
+        Ok(HandleResult::NotHandled)
     }
 }
 
-impl KeyHandler for KeyPress {
-    fn process_event(&self, event: &InputEvent, view: ProcView) -> ProcessResult {
-        if event.code() == self.key.code() {
-            (self.action)(view);
-            ProcessResult::Consumed
-        } else {
-            ProcessResult::NotConsumed
-        }
-    }
+trait KeyEventHandler {
+    fn handle_event(&self, proc: &mut Processor) -> Result<HandleResult>;
 }
 
-enum KH {
-    Key(KeyPress),
-    UpKey(KeyPress),
-    Chord(Chord),
-}
-
-struct Chord {
-    action: BoxAction,
+enum HandleResult {
+    Handled,
+    NotHandled,
 }
 
 struct Processor<'a> {
-    devices: EvdevDevices,
-    // key_presses: &'a [KeyPress],
-    state: ProcState<'a>,
-}
-
-struct ProcView<'v, 'p> {
-    state: &'v mut ProcState<'p>,
-    virt_kb: &'v mut VirtualDevice,
-}
-
-struct EvdevDevices {
     input_kb: Device,
-    virt_kb: VirtualDevice,
+    output_kb: VirtualDevice,
+    //////// Not technically needed to store layers in Processor
+    layers: Vec<Layer>,
+    // Using a usize, as we cannot store a reference to the layer
+    // Possible alternative: Rc<Box<Layer>>
+    active_layer_id: usize,
+    // active_layer: Option<&'a mut Layer>,
+    _phantom_data: std::marker::PhantomData<&'a ()>, // temp, while refactoring
 }
 
-#[derive(Default)]
-struct ProcState<'a> {
-    active_layer: Option<&'a Layer<'a>>,
+struct ProcView<'v> {
+    active_layer: &'v usize,
+    output_kb: &'v VirtualDevice,
+}
+
+impl<'a> ProcView<'a> {
+    fn set_active_layer(layer: &mut Layer) {}
 }
 
 impl<'a> Processor<'a> {
-    // fn set_active_layer(&mut self, layer: &'a Layer) {
-    //     self.state.active_layer = Some(layer);
-    // }
-
-    fn run(&mut self) -> Result<()> {
-        ///// use get_key_state to figure out the initial state and release any pressed key:
-        //self.input_kb.get_key_state()
-
-        for event in self.devices.input_kb.fetch_events()? {
-            if event.event_type() == EventType::KEY {
-                Self::process_key_event(&mut self.state, &mut self.devices.virt_kb, event)?;
-            } else {
-                self.devices.virt_kb.emit(&[event])?;
-            }
-        }
-        Ok(())
+    fn new(input_kb: Device, output_kb: VirtualDevice, layers: Vec<Layer>) -> Self {
+        assert!(layers.len() > 0);
+        let first_layer_id = layers[0].id;
+        let p = Self {
+            input_kb,
+            output_kb,
+            layers: layers,
+            active_layer_id: first_layer_id,
+            _phantom_data: PhantomData,
+        };
+        // p.active_layer = Some(p.layers.get_mut(0).unwrap());
+        p
     }
 
-    fn process_key_event(
-        state: &mut ProcState,
-        virt_kb: &mut VirtualDevice,
-        event: InputEvent,
-    ) -> Result<ProcessResult> {
-        if let Some(layer) = state.active_layer {
-            for kp in layer.handlers {
-                if kp.process_event(&event, ProcView { state, virt_kb }) == ProcessResult::Consumed
-                {
-                    break;
-                    // (kp.action)(ProcView { state, virt_kb });
-                }
-            }
-            // for kp in layer.key_presses {
-            //     (kp.action)(ProcView { state, virt_kb });
-            // }
-        }
-        virt_kb.emit(&[event])?;
-        Ok(ProcessResult::Consumed)
+    fn run(&mut self) {
+        //// cannot pass self to action, as we will already have a mut borrow?
+        // self.active_layer = Some(&mut self.layers[0]);
+        //self.set_active_layer(&self.layers[0]);
+        Self::reset_layer(&mut self.layers[self.active_layer_id]);
+        self.active_layer_id = 0;
+        for event in self.input_kb.fetch_events() {}
+        // let s = self.layers[0].handlers[0].as_mut().handle_event(&mut *self);
+        // self.active_layer = Some(&mut self.layers[0]);
+    }
+
+    // fn set_active_layer(&mut self, new_layer: &Layer) {
+    fn set_active_layer(&mut self, new_layer_id: usize) -> Result<()> {
+        // Reset the loaded layer instead?
+        let old_layer_id = self.active_layer_id;
+        Self::reset_layer(&mut self.layers.get_mut(old_layer_id).unwrap());
+        self.active_layer_id = new_layer_id;
+        Ok(())
+    }
+    
+    fn reset_layer(layer: &mut Layer) {
+        //// go through layer and reset handlers/state
+        //// Reset press state from old layer? All except layer button should flush? chords can either just reset - and the keys will then send spurious releases, or they can send press+release for the captured buttons
     }
 }
 
 fn main() -> Result<()> {
-    // let space_chord = {}
-
-    const NAV_LAYER: Layer = Layer { handlers: &[] };
-
-    // let num_key = KeyPress {
-    //     key: Key::KEY_F,
-    //     action: Box::new(|state| {
-    //         state.active_layer = Some(&NAV_LAYER);
-    //     }),
-    // };
-    let num_key = KeyPress::new(
-        Key::KEY_F,
-        ba(&|pv: ProcView| pv.state.active_layer = Some(&NAV_LAYER)),
-    );
-    let test_key = KeyPress::new(
-        Key::KEY_G,
-        // Box::new(&|state: &mut ProcState, virt_kb: &mut VirtualDevice| {
-        Box::new(&|pv: ProcView| {
-            pv.virt_kb.emit(&[]).ok();
-        }),
-    );
-    let first_layer = Layer {
-        handlers: &[&test_key, &num_key],
-    };
-
     let mut virt_kb = VirtualDeviceBuilder::new()?
         .name("keyremap")
         // .with_keys(&keys)?
         .build()?;
-
     let mut unguarded_dev =
         Device::open("/dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-kbd")?;
-    let mut p = Processor {
-        devices: EvdevDevices {
-            input_kb: unguarded_dev,
-            virt_kb,
-        },
-        state: ProcState {
-            ..Default::default()
-        },
-    };
-    p.state.active_layer = Some(&NAV_LAYER);
-    p.run();
-    // match p.process() {
-    //     ProcessResult::KeyPressed(kp) => ((kp.action)(&mut p.state)),
-    //     ProcessResult::None => (),
-    // }
+
+    let mut lf = LayerFactory::new();
+    const FIRST_LAYER_ID: usize = 0;
+    let mut first_layer = lf.create_layer();
+
+    first_layer.add_key_press(
+        Key::KEY_F,
+        Box::new(|p| {
+            // pv.active_layer = first_layer.id;
+            Processor::set_active_layer(p, FIRST_LAYER_ID)?;
+            Ok(())
+        }),
+    );
+    let id = first_layer.id;
+
+    let layers = vec![first_layer];
+    let mut proc = Processor::new(unguarded_dev, virt_kb, layers);
+
+    // let l = proc.layers.get(id).unwrap();
+    // proc.set_active_layer(&l);
+
     Ok(())
 }
