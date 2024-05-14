620f5ef2f072ca9d657689b1454e130ebc50d2d8
Thomas Axelsson <thomasa88@gmail.com>
2024-05-14 07:07:32 +0200

    Callback compiles

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::{cell::OnceCell, marker::PhantomData, sync::Once, vec};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use evdev::{
-    uinput::{VirtualDevice, VirtualDeviceBuilder},
-    Device, Key,
+    uinput::{VirtualDevice, VirtualDeviceBuilder}, Device, InputEvent, Key
 };
 use tracing_subscriber::layer;
 
@@ -50,13 +49,14 @@ struct KeyPress {
 }
 
 impl KeyEventHandler for KeyPress {
-    fn handle_event(&self, proc: &mut Processor) -> Result<HandleResult> {
+    fn handle_event(&self, proc: ProcView) -> Result<HandleResult> {
         Ok(HandleResult::NotHandled)
     }
 }
 
 trait KeyEventHandler {
-    fn handle_event(&self, proc: &mut Processor) -> Result<HandleResult>;
+    // fn handle_event(&self, proc: &mut Processor) -> Result<HandleResult>;
+    fn handle_event(&self, proc: ProcView) -> Result<HandleResult>;
 }
 
 enum HandleResult {
@@ -69,20 +69,23 @@ struct Processor<'a> {
     output_kb: VirtualDevice,
     //////// Not technically needed to store layers in Processor
     layers: Vec<Layer>,
-    // Using a usize, as we cannot store a reference to the layer
-    // Possible alternative: Rc<Box<Layer>>
+    // Using a usize, as we cannot store a reference to the layer:
+    //  Tried to use reference both in Processor and in a key action closure:
+    //   cannot borrow `first_layer` as mutable more than once at a time
+    // Possible alternative: Rc<Box<Layer>>?
     active_layer_id: usize,
     // active_layer: Option<&'a mut Layer>,
     _phantom_data: std::marker::PhantomData<&'a ()>, // temp, while refactoring
 }
 
 struct ProcView<'v> {
+    // event: &'v InputEvent,
     active_layer: &'v usize,
     output_kb: &'v VirtualDevice,
 }
 
 impl<'a> ProcView<'a> {
-    fn set_active_layer(layer: &mut Layer) {}
+    // fn set_active_layer(layer: &mut Layer) {}
 }
 
 impl<'a> Processor<'a> {
@@ -100,26 +103,59 @@ impl<'a> Processor<'a> {
         p
     }
 
-    fn run(&mut self) {
+    fn run(&mut self) -> Result<()> {
         //// cannot pass self to action, as we will already have a mut borrow?
         // self.active_layer = Some(&mut self.layers[0]);
         //self.set_active_layer(&self.layers[0]);
-        Self::reset_layer(&mut self.layers[self.active_layer_id]);
-        self.active_layer_id = 0;
-        for event in self.input_kb.fetch_events() {}
+        // Self::reset_layer(&mut self.layers[self.active_layer_id]);
+        // self.active_layer_id = 0;
+        // wrap in a loop?
+        for event in self.input_kb.fetch_events()? {
+            let active_layer = self
+                .layers
+                .get_mut(self.active_layer_id)
+                .context("No active layer set")?;
+            let mut layer_switch = false;
+            let old_layer_id = self.active_layer_id;
+            for handler in &active_layer.handlers {
+                handler.handle_event(ProcView {
+                    active_layer: &self.active_layer_id,
+                    output_kb: &self.output_kb,
+                })?;
+                if self.active_layer_id != old_layer_id {
+                    layer_switch = true;
+                    break;
+                    // Layer switch!
+                    // Self::reset_layer(
+                    //     self.layers
+                    //         .get_mut(old_layer_id)
+                    //         .context("Invalid layer id: ")?,
+                    // );
+                }
+            }
+            if layer_switch {
+                // New layer ID already set by callback
+                Self::reset_layer(
+                    self.layers
+                        .get_mut(old_layer_id)
+                        .context("Invalid layer id: ")?,
+                );
+            }
+        }
         // let s = self.layers[0].handlers[0].as_mut().handle_event(&mut *self);
         // self.active_layer = Some(&mut self.layers[0]);
+        Ok(())
     }
 
     // fn set_active_layer(&mut self, new_layer: &Layer) {
-    fn set_active_layer(&mut self, new_layer_id: usize) -> Result<()> {
-        // Reset the loaded layer instead?
-        let old_layer_id = self.active_layer_id;
-        Self::reset_layer(&mut self.layers.get_mut(old_layer_id).unwrap());
-        self.active_layer_id = new_layer_id;
-        Ok(())
-    }
-    
+    // fn set_active_layer(&mut self, new_layer_id: usize) -> Result<()> {
+    //     // Reset the loaded layer instead?
+    //     let old_layer_id = self.active_layer_id;
+    //     Self::reset_layer(&mut self.layers.get_mut(old_layer_id).unwrap());
+    //     self.active_layer_id = new_layer_id;
+    //     Ok(())
+    // }
+
     fn reset_layer(layer: &mut Layer) {
         //// go through layer and reset handlers/state
         //// Reset press state from old layer? All except layer button should flush? chords can either just reset - and the keys will then send spurious releases, or they can send press+release for the captured buttons
@@ -137,22 +173,23 @@ fn main() -> Result<()> {
     let mut lf = LayerFactory::new();
     const FIRST_LAYER_ID: usize = 0;
     let mut first_layer = lf.create_layer();
+    first_layer.id = FIRST_LAYER_ID;
 
     first_layer.add_key_press(
         Key::KEY_F,
         Box::new(|p| {
-            // pv.active_layer = first_layer.id;
-            Processor::set_active_layer(p, FIRST_LAYER_ID)?;
+            p.active_layer_id = FIRST_LAYER_ID;
             Ok(())
         }),
     );
-    let id = first_layer.id;
 
     let layers = vec![first_layer];
     let mut proc = Processor::new(unguarded_dev, virt_kb, layers);
 
     // let l = proc.layers.get(id).unwrap();
     // proc.set_active_layer(&l);
+
+    proc.run();
 
     Ok(())
 }
