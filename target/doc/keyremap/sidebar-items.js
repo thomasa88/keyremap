fe230db39df63c9015b93d51451a9c4343f356c6
Thomas Axelsson <thomasa88@gmail.com>
2024-05-29 20:27:45 +0200

    Docs

---
+++ target/doc/keyremap/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["QUICK_REPEAT_DELAY"],"enum":["HandlerEvent","HandlerState","KeyAction","KeyEventValue"],"fn":["main"],"mod":["chord","longmod","single_key"],"struct":["Args","LayerConfig","NiceKeyInputEvent","ProcView","Processor","TaggedHandler"],"trait":["KeyEventHandler"],"type":["ActionFn","HandlerBox","ResetFn"]};
